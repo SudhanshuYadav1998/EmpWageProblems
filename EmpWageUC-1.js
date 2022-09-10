23438821589aa9ab8cc916c6c65c178053ebed5c
SudhanshuYadav1998 <sy7040@gmail.com>
2022-09-10 23:01:32 +0530

    Added Employee Attendence  file

---
+++ EmpWageUC-1.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Employee Wage Calculator")
+let num=Math.floor(Math.random()*2);
+if(num===0)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}
